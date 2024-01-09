c9649ab63e3b859d05b948c7d21ad7de00ee96ad
Prathamesh Rokade <prathameshr955@gmail.com>
2024-01-09 22:07:40 +0530

    Created App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Taskly',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.red,
       ),
       home: HomePage(),
     );
